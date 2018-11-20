ea6edf667a09ffb31ebcc69143239417b08fbdc4
Caio Gomes <caiocsgomes@gmail.com>
2018-11-20 18:36:46 -0200

    Adding method to find bus to the circuit class

--- Circuit.cpp
+++ Circuit.cpp
@@ -1,8 +1,11 @@
 #include "stdafx.h"
 #include "Circuit.h"
 
-
 Circuit::Circuit() {};
 
-
 Circuit::~Circuit() {};
+
+Bus* Circuit::getBusById(int id) {
+	if (this->busVector[id - 1]) return this->busVector[id - 1];
+	return nullptr;
+}
--- Circuit.h
+++ Circuit.h
@@ -7,6 +7,7 @@ class Circuit
 public:
 	Circuit();
 	~Circuit();
+	Bus* getBusById(int id);
 	std::vector<Branch*> branchVector;
 	std::vector<Bus*> busVector;
 };
