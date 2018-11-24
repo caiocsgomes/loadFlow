b310f2269a98c9d07a1f29c4ce74acc2c95a524c
Caio Gomes <caiocsgomes@gmail.com>
2018-11-24 15:26:12 -0200

    Adding getBusById method in the circuit class

--- Circuit.cpp
+++ Circuit.cpp
@@ -5,6 +5,8 @@ Circuit::Circuit() {};
 
 Circuit::~Circuit() {};
 
+//The buses in the txt come with the id in a linear order (1, 2, 3..., n)
+//So we just need to return the bus in the position (id - 1), considering the vector has a zero based index 
 Bus* Circuit::getBusById(int id) {
 	if (this->busVector[id - 1]) return this->busVector[id - 1];
 	return nullptr;
